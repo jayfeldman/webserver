e850591467fc5b9179dd353027234045ab5baa78
Jay Feldman <jayami517@gmail.com>
2019-08-18 10:02:04 -0700

    modified:   webserver.py

--- webserver.py
+++ webserver.py
@@ -14,22 +14,3 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
-
-
-# In[ ]:
-
-
-help
-
-
-# In[ ]:
-
-
-help(print)
-
-
-# In[ ]:
-
-
-
-
